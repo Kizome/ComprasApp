4e56ee4af9b0b96e797904c395fc3a0d62b39f35
Omar <47725077+Kizome@users.noreply.github.com>
2019-11-15 22:07:40 +0100

    Aplicacion definitiva

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBRW5nfof5dr4N_rdiSXGFTcNKKOPeySxY",
+    authDomain: "appcompras-b3c3f.firebaseapp.com",
+    databaseURL: "https://appcompras-b3c3f.firebaseio.com",
+    projectId: "appcompras-b3c3f",
+    storageBucket: "appcompras-b3c3f.appspot.com",
+    messagingSenderId: "1046490358776",
+    appId: "1:1046490358776:web:00347c82751a902ac06c0d",
+    measurementId: "G-JTRQ1FRV84"
+  }
 };
